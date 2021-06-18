8caa285972a5786bffc75abdb60871ac6235eb5c
Galih Anggoro Prasetya <83481679+galihap76@users.noreply.github.com>
2021-06-18 22:58:36 +0700

    latihan1.pas

---
+++ latihan-pascalku/latihan1.pas
@@ -0,0 +1,15 @@
+program latihan1;
+
+{$APPTYPE CONSOLE}
+
+uses
+  SysUtils;
+
+begin
+  { TODO -oUser -cConsole Main : Insert code here }
+
+  writeln('Hello World');
+  writeln('Program pertama saya dalam pascal');
+  readln;
+
+end.
