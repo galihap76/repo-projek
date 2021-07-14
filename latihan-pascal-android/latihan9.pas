22f7e5d4bd8722826f731e27273def68b8dbf2d4
Galih Anggoro Prasetya <83481679+galihap76@users.noreply.github.com>
2021-07-15 00:20:32 +0700

    Latihan pascal android.

---
+++ latihan-pascal-android/latihan9.pas
@@ -0,0 +1,26 @@
+program latihan9;
+uses crt;
+
+procedure CetakHello;
+begin
+writeln('Hello World');
+end;
+
+procedure CetakNamaku;
+begin
+writeln('Hello Galih');
+end;
+
+procedure JumlahPenambahan;
+begin
+writeln(5*5);
+end;
+
+begin
+  CetakHello;
+  CetakNamaku;
+  JumlahPenambahan;
+   
+  
+  readln;
+end.
