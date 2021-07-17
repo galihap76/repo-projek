13523f54160d8f2288d25341735502548fc7a3f0
Galih Anggoro Prasetya <83481679+galihap76@users.noreply.github.com>
2021-07-17 20:44:21 +0700

    pertemuan 17.

---
+++ latihan-phpku/pertemuan17/index.php
@@ -0,0 +1,65 @@
+<?php
+session_start();
+
+if(!isset($_SESSION["login"])){
+	header("Location: login.php");
+	exit;
+}
+require 'functions.php';
+$mahasiswa = query("SELECT * FROM mahasiswa");
+
+// tombol cari ditekan
+if(isset($_POST["cari"])){
+	$mahasiswa = cari ($_POST["keyword"]);
+}
+?>
+
+
+
+
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+	<title>Halaman Admin</title>
+</head>
+<a href="logout.php">Logout</a>
+
+<body>
+<h1>Daftar Mahasiswa</h1>
+<a href="tambah.php">Tambah Data Mahasiswa</a>
+<br><br>
+<form action="" method="post">
+	<input type="text" name="keyword" size="40" autofocus placeholder="masukkan keyword pencarian..." autocomplete="off">
+	<button type="submit" name="cari">Cari!</button>
+</form>
+<table border="1" cellpadding="10" cellspacing="0">
+	<tr>
+		<th>No.</th>
+		<th>Aksi</th>
+		<th>Gambar</th>
+		<th>NRP</th>
+		<th>Nama</th>
+		<th>Email</th>
+		<th>Jurusan</th>
+	</tr>
+	<?php $i = 1; ?>
+	<?php foreach($mahasiswa as $row) : ?>
+	<tr>
+		<td><?= $i; ?></td>
+		<td>
+			<a href="ubah.php?id=<?= $row["id"]; ?>">ubah</a> 
+			<a href="hapus.php?id=<?php echo $row["id"];?>" onClick="return confirm('yakin?');">hapus</a>
+		</td>
+		<td><img src="img/<?= $row["gambar"]; ?>" width="100"></td>
+		<td><?= $row["nrp"]; ?></td>
+		<td><?= $row["nama"]; ?></td>
+		<td><?= $row["email"]; ?></td>
+		<td><?= $row["jurusan"]; ?></td>
+	</tr>
+	<?php $i++; ?>
+	<?php endforeach; ?>
+</table>
+</body>
+</html>
