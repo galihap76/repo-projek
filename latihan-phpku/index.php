3ad3e139cadb0aa0617fd97491884a51026e4987
Galih Anggoro Prasetya <83481679+galihap76@users.noreply.github.com>
2021-07-17 19:21:52 +0700

    Pertemuan 1.

---
+++ latihan-phpku/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Belajar Php</title>
+</head>
+
+<body>
+<?php
+	$nilai1=5;
+	$nilai2=5;
+	$hasil=$nilai1+$nilai2;
+	echo ($hasil);
+?>
+</body>
+</html>
