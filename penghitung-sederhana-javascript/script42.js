c8fb74448d20076d924c8a433c33c8b8c6b4a16b
Galih Anggoro Prasetya <83481679+galihap76@users.noreply.github.com>
2021-07-17 19:05:48 +0700

    Pembaruan Kode.

--- penghitung-sederhana-javascript/script42.js
+++ penghitung-sederhana-javascript/script42.js
@@ -1,63 +1,38 @@
 //javascript document
-let tambah=document.getElementsByName('tambah')[0];
-let kurang=document.getElementsByName('kurang')[0];
-let kali=document.getElementsByName('kali')[0];
-let bagi=document.getElementsByName('bagi')[0];
-let reset=document.getElementsByName('reset')[0];
-let hitung=document.getElementById('hitung');
-let form=document.getElementById('form');
-tambah.addEventListener('click', cekhasilTambah);
-kurang.addEventListener('click', cekhasilKurang);
-kali.addEventListener('click', cekhasilKali);
-bagi.addEventListener('click', cekhasilBagi);
-reset.addEventListener('click', resetHasil);
+let angka1 = document.getElementById('angka1');
+let angka2 = document.getElementById('angka2');
+let hasil = document.getElementById('hasil');
+let tambah = document.getElementsByName('tambah')[0];
+let kurang = document.getElementsByName('kurang')[0];
+let kali = document.getElementsByName('kali')[0];
+let bagi = document.getElementsByName('bagi')[0];
+let reset = document.getElementsByName('reset')[0];
+let form = document.getElementsByTagName('form')[0];
 
-function cekhasilTambah(e){
-    if(form.hitung.value){
-		let hasilpenambahan=eval(form.hitung.value);
-		 hasilpenambahan + hasilpenambahan;
-		form.hasil.value=hasilpenambahan;
-	}else if(form.hitung.value===""){
-		alert('Maaf jangan kosong.');
-	}
-	e.preventDefault();
+tambah.addEventListener('click', btnTambah);
+kurang.addEventListener('click', btnKurang);
+kali.addEventListener('click', btnKali);
+bagi.addEventListener('click', btnBagi);
+reset.addEventListener('click', btnReset);
+function btnTambah(){
+	tambah = parseInt(angka1.value) + parseInt(angka2.value);
+	form.hasil.value = tambah;
+}
+function btnKurang(){
+	kurang = parseInt(angka1.value) - parseInt(angka2.value);
+	form.hasil.value = kurang;
+}
+function btnKali(){
+	kali = parseInt(angka1.value) * parseInt(angka2.value);
+	form.hasil.value = kali;
+}
+function btnBagi(){
+	bagi = parseInt(angka1.value) / parseInt(angka2.value);
+	form.hasil.value = bagi;
+}
+function btnReset(){
+	form.hasil.value = "";
+	form.angka1.value = "";
+	form.angka2.value = "";
 }
 
-function cekhasilKurang(e){
-	if(form.hitung.value){
-		let hasilpengurangan=eval(form.hitung.value);
-		hasilpengurangan - hasilpengurangan;
-		form.hasil.value=hasilpengurangan;
-	}else if(form.hitung.value===""){
-		alert('Maaf jangan kosong.');
-	}
-	e.preventDefault();
-}
-
-function cekhasilKali(e){
-	if(form.hitung.value){
-		let hasilKali=eval(form.hitung.value);
-		hasilKali * hasilKali;
-		form.hasil.value=hasilKali;
-	}else if(form.hitung.value===""){
-		alert('Maaf jangan kosong.');
-	}
-	e.preventDefault();
-}
-
-function cekhasilBagi(e){
-	if(form.hitung.value){
-		let hasilBagi=eval(form.hitung.value);
-		hasilBagi / hasilBagi;
-		form.hasil.value=hasilBagi;
-	}else if(form.hitung.value===""){
-		alert('Maaf jangan kosong.');
-	}
-	e.preventDefault();
-}
-
-function resetHasil(e){
-	form.hitung.value="";
-	form.hasil.value="";
-	e.preventDefault();
-}
