f6e22c1d959c6df69135351b948bdcbb174800ca
Galih Anggoro Prasetya <83481679+galihap76@users.noreply.github.com>
2021-07-21 15:49:44 +0700

    perubahan script.

--- aplikasi-CRUD-RSIA/login.php
+++ aplikasi-CRUD-RSIA/login.php
@@ -64,7 +64,7 @@ if(isset($_COOKIE["WWWDI"]) && isset($_COOKIE["WWWEYK"])){
         <title>Administrator Login</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<!--        <link href="css/styles.css" rel="stylesheet" />-->
+        <link href="styles.css" rel="stylesheet" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
 <!--        My Css-->
    <style>
@@ -129,6 +129,6 @@ if(isset($_COOKIE["WWWDI"]) && isset($_COOKIE["WWWEYK"])){
         </div>
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="script-app.js"></script>
     </body>
 </html>
+
