3824c021f9a4aa0b0417e97588a79ddf941ec419
Galih Anggoro Prasetya <83481679+galihap76@users.noreply.github.com>
2021-08-15 15:42:05 +0700

    latihan ku.

---
+++ python/latihan1.py
@@ -0,0 +1,13 @@
+#print
+print('Hello World');
+
+#penjumlahan
+a = 10
+b = 10
+
+print('Penjumlahan a + b = ', a + b);
+
+namasaya = 'galih anggoro prasetya'
+umursaya = 17
+
+print('nama saya adalah ', namasaya, 'umur saya ', umursaya, ' tahun')
---
+++ python/latihan2.py
@@ -0,0 +1,15 @@
+text1 = 'Hello World!'
+
+text2 = "Hello World!"
+
+text3 = "Database"
+
+text4 = "Python"
+
+text5 = "Python Hacking Tool"
+
+print(text1)
+print(text2)
+print(text3[3:8])
+print(text4[4:6])
+print(text5[15:19])
