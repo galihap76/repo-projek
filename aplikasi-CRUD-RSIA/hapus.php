60b2ba8e2734357d99e71496a934410c72ce936e
Galih Anggoro Prasetya <83481679+galihap76@users.noreply.github.com>
2021-09-14 15:19:41 +0700

    delete syntax.

--- aplikasi-CRUD-RSIA/hapus.php
+++ aplikasi-CRUD-RSIA/hapus.php
@@ -7,7 +7,7 @@ if(!isset($_SESSION["login"])){
 	header("Location: login.php");
 	exit;
 }
-$id= abs((int)$_GET["id"];
+$id= $_GET["id"];
          
    //cek apakah data berhasil di hapus atau tidak
 if(hapus($id)>0){
