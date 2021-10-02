8c26ad6cafb1ec2080aa561aa84a5794d83d1025
Galih Anggoro Prasetya <83481679+galihap76@users.noreply.github.com>
2021-10-02 21:54:45 +0700

    latihan ku.

---
+++ python/latihan7.py
@@ -0,0 +1,11 @@
+namaku = "Halo galih anggoro prasetya"
+
+def aritmatika(a,b):
+    hasil = a + b
+
+
+nama = "galih"
+password = 76
+
+
+
---
+++ python/program_hitung_sederhana.py
@@ -0,0 +1,11 @@
+#program hitung sederhana python
+nilai = int(input("Masukkan nilai anda : "))
+
+
+if nilai > 90:
+    print('Anda mendapatkan nilai A.')
+elif nilai > 50:
+    print('Anda mendapatkan nilai B.')
+else:
+    print('Nilai anda belum memuaskan')
+    
